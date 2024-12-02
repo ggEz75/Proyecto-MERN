01f201536bafe0bd834cc56992c9c1161692e719
ggEz75 <guillegomez575@gmail.com>
2024-12-02 02:57:43 -0300

    Registro de usuarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,29 @@
+import {pool} from '../db.js'
+import bcrypt from 'bcrypt'
 
 export const sign = (req, res) => res.send('ingresando');
 
-export const signUp = (req, res) => res.send('Registrando');
+export const signUp = async (req, res) => {
+    const { name, email, password} = req.body;
+
+    try {
+
+        const hashedPassword = await bcrypt.hash(password, 10) // el numero ejecuta el numero de veces el algoritmo, es recomendable usar de 10 a 15
+        console.log(hashedPassword)
+
+        const result = await pool.query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *', [name, email, hashedPassword]
+        );
+        console.log(result);
+    
+        return res.json(result.rows[0]);
+    } catch (error) {
+        if (error.code === "23505"){
+            return res.status(400).json({
+                message: "El correo ya esta registrado"
+            });
+        }
+    }
+};
 
 export const signOut = (req, res) => res.send('cerrando sesion');
 
